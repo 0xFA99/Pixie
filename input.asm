5746088695d5132478c8bef6fea8f3e867cbe049
0xFA99 <fajar.nur.h@pm.me>
2025-04-03 14:43:32 +0700

    fix player movement and little optimized

--- input.asm
+++ input.asm
@@ -1,15 +1,15 @@
-; rdi = Player*
+; ============== PARAMETERS ==============
+; [rbp -  8]        = Player*
+; [rbp - 12]        = float frameTime
+; [rbp - 16]        = float gravity
 
-; [rbp - 8], Player*
-; [rbp - 12], int runDir
-; [rbp - 13], bool isJump
-
-; [rbp - 12], frameTime
-; [rbp - 16], gravity
-; [rbp - 20], runDir
-; [rbp - 21], isJump ; 24
-
-; [rbp - 32], Vector2 newPos
+; ============== VARIABLES ===============
+; [rbp - 20]        = int runDirection
+; [rbp - 21]        = bool isJump
+; [rbp - 22]        = bool keyLeftPressed
+; [rbp - 23]        = bool keyRightPressed
+; [rbp - 28]        = float newPosition.y
+; [rbp - 32]        = float newPosition.x
 
 _InputPlayer:
     push rbp
@@ -22,6 +22,7 @@ _InputPlayer:
 
     mov dword [rbp - 20], 0
 
+    ; Check if KEY_UP is pressed
     mov edi, 265                ; KEY_UP
     call IsKeyDown
     mov [rbp - 21], al
@@ -42,8 +43,8 @@ _InputPlayer:
     mov dword [rbp - 20], -1
 
 .startCheckState:
-    mov rax, [rbp - 8]
-    mov eax, [rax + 40]
+    mov rdi, [rbp - 8]
+    mov eax, [rdi + 40]
 
     cmp eax, STATE_IDLE
     je .stateIdle
@@ -57,20 +58,18 @@ _InputPlayer:
     cmp eax, STATE_FALL
     je .stateJumpAndFall
 
-    jmp .return
+    jmp .endCheckState
 
 .stateIdle:
     cmp byte [rbp - 21], 0
     je .stateIdleToRun
 
 ;   player->status.state = STATE_JUMP
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_JUMP
+    mov dword [rdi + 40], STATE_JUMP
 
 ;   player->movement.velocity.y = player->movement.acceleration
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 32]
-    movss [rax + 28], xmm0
+    movss xmm0, [rdi + 32]
+    movss [rdi + 28], xmm0
 
     jmp .endCheckState
 
@@ -79,31 +78,29 @@ _InputPlayer:
     je .stateIdleToIdle
 
 ;   player->status.state = STATE_RUN
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_RUN
+    mov dword [rdi + 40], STATE_RUN
 
 ;   player->status.direction = runDir > 0
-    cmp dword [rbp - 20], 0
+    ; cmp dword [rbp - 20], 0
     setg al
     movzx edx, al
-    mov rax, [rbp - 8]
-    mov [rax + 44], edx
+    mov [rdi + 44], edx
 
 ;   player->movement.velocity.x = runDir * player->movement.speed
     pxor xmm1, xmm1
     cvtsi2ss xmm1, [rbp - 20]
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 36]
+
+    movss xmm0, [rdi + 36]
+
     mulss xmm0, xmm1
-    movss [rax + 24], xmm0
+    movss [rdi + 24], xmm0
 
     jmp .endCheckState
 
 .stateIdleToIdle:
 ;   player->movement.velocity = 0
     pxor xmm0, xmm0
-    mov rax, [rbp - 8]
-    movsd [rax + 24], xmm0
+    movsd [rdi + 24], xmm0
 
     jmp .endCheckState
 
@@ -112,13 +109,11 @@ _InputPlayer:
     je .stateRunToIdle
 
 ;   player->status.state = STATE_JUMP
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_JUMP
+    mov dword [rdi + 40], STATE_JUMP
 
 ;   player->movement.velocity.y = player->movement.acceleration
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 32]
-    movss [rax + 28], xmm0
+    movss xmm0, [rdi + 32]
+    movss [rdi + 28], xmm0
 
     jmp .endCheckState
 
@@ -127,13 +122,11 @@ _InputPlayer:
     jne .stateRunToRun
 
 ;   player->status.state = STATE_IDLE
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_IDLE
+    mov dword [rdi + 40], STATE_IDLE
 
 ;   player->movement.velocity.x = 0.0f
     pxor xmm0, xmm0
-    mov rax, [rbp - 8]
-    movss [rax + 24], xmm0
+    movss [rdi + 24], xmm0
 
     jmp .endCheckState
 
@@ -142,16 +135,14 @@ _InputPlayer:
     cmp dword [rbp - 20], 0
     setg al
     movzx edx, al
-    mov rax, [rbp - 8]
-    mov [rax + 28], edx
+    mov [rdi + 28], edx
 
 ;   player->movement.velocity.x = runDir * player->movement.speed
     pxor xmm1, xmm1
     cvtsi2ss xmm1, [rbp - 20]
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 38]
+    movss xmm0, [rdi + 36]
     mulss xmm0, xmm1
-    movss [rax + 24], xmm0
+    movss [rdi + 24], xmm0
 
     jmp .endCheckState
 
@@ -163,30 +154,26 @@ _InputPlayer:
     cmp dword [rbp - 20], 0
     setg al
     movzx edx, al
-    mov rax, [rbp - 8]
-    mov [rax + 44], edx
+    mov [rdi + 44], edx
 
 ;   player->movement.velocity.x = runDir * player->movement.speed
     pxor xmm1, xmm1
     cvtsi2ss xmm1, [rbp - 20]
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 38]
+    movss xmm0, [rdi + 36]
     mulss xmm0, xmm1
-    movss [rax + 24], xmm0
+    movss [rdi + 24], xmm0
 
 .stateJumpToJumpOrFall:
 ;   player->movement.velocity.x = runDir * player->movement.speed * frameTime
     pxor xmm1, xmm1
     cvtsi2ss xmm1, [rbp - 20]
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 38]
+    movss xmm0, [rdi + 36]
     mulss xmm0, xmm1
     mulss xmm0, [rbp - 12]
-    movss [rax + 24], xmm0
+    movss [rdi + 24], xmm0
 
 .checkIfPlayerJump:
-    mov rax, [rbp - 8]
-    movss xmm0, [rax + 28]
+    movss xmm0, [rdi + 28]
     pxor xmm1, xmm1
     comiss xmm0, xmm1
     jnb .playerStillJump
@@ -194,37 +181,28 @@ _InputPlayer:
     jmp .endCheckState
 
 .playerStillJump:
-    mov rax, [rbp - 8]
-    mov eax, [rax + 40]
+    mov eax, [rdi + 40]
     cmp eax, STATE_JUMP
     jne .endCheckState
 
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_FALL
+    mov dword [rdi + 40], STATE_FALL
 
 .endCheckState:
 ;   player->movement.velocity.y += gravity * frameTime
-    mov rax, [rbp - 8]
-    movss xmm1, [rax + 28]
+    movss xmm1, [rdi + 28]
     movss xmm0, [rbp - 16]
     mulss xmm0, [rbp - 12]
     addss xmm0, xmm1
-    movss [rax + 28], xmm0
+    movss [rdi + 28], xmm0
 
-    movss xmm0, [rbp - 12]
-    mov rax, [rbp - 8]
-    mov rax, [rax + 24]
-    movaps xmm1, xmm0
-    movq xmm0, rax
+    movss xmm1, [rbp - 12]
+
+    movsd xmm0, [rdi + 24]
     call Vector2Scale
-    movq rdx, xmm0
-    mov rax, [rbp - 8]
-    mov rax, [rax + 16]
-    movq xmm1, rdx
-    movq xmm0, rax
+
+    movsd xmm1, [rdi + 16]
     call Vector2Add
-    movq rax, xmm0
-    mov [rbp - 32], rax
+    movsd [rbp - 32], xmm0
 
     movss xmm0, [rbp - 28]
     pxor xmm1, xmm1
@@ -234,28 +212,23 @@ _InputPlayer:
     pxor xmm0, xmm0
     movss [rbp - 28], xmm0
 
-    mov rax, [rbp - 8]
     pxor xmm0, xmm0
-    movss [rax + 28], xmm0
+    movss [rdi + 28], xmm0
 
-    mov rax, [rbp - 8]
-    mov eax, [rax + 40]
+    mov eax, [rdi + 40]
     cmp eax, STATE_FALL
     je .setToIdle
 
-    mov rax, [rbp - 8]
-    mov eax, [rax + 40]
+    mov eax, [rdi + 40]
     cmp eax, STATE_JUMP
     jne .updatePosition
 
 .setToIdle:
-    mov rax, [rbp - 8]
-    mov dword [rax + 40], STATE_IDLE
+    mov dword [rdi + 40], STATE_IDLE
 
 .updatePosition:
-    mov rax, [rbp - 8]
     mov rdx, [rbp - 32]
-    mov [rax + 16], rdx
+    mov [rdi + 16], rdx
 
 .return:
     add rsp, 48
